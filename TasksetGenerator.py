1f6d3b653c0074038c8f9ede7b0004e8048f73c3
djtfoo <djt.foo@gmail.com>
2022-04-10 04:35:35 +0800

    TasksetGenerator able to accept either 'uniform' or 'normal' distribution option for task generation, and able to generate deadlines for each task

--- TasksetGenerator.py
+++ TasksetGenerator.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+import random
 
 # n = 200     # no. tasks
 # sys_util = 1.0  # % utilisation of the system
@@ -18,12 +19,14 @@ import sys
 # generalise it so it can also generate task set for EnSuRe, then to generate for FEST it is a specific restriction?
 
 class TasksetGenerator:
-    def __init__(self, n, frame_duration, sys_util, precision_dp, lp_hp_ratio):
+    def __init__(self, distribution, n, frame_duration, sys_util, precision_dp, lp_hp_ratio):
         """
-        n = no. tasks per set
-        frame_duration = frame deadline (ms)
-        sys_util = system utilisation (%)
+        distribution: "uniform" or "normal"
+        n: no. tasks per set
+        frame_duration: frame deadline (ms)
+        sys_util: system utilisation (%)
         """
+        self.distribution = distribution
         self.n = n
         self.sys_util = sys_util
         self.frame_duration = frame_duration
@@ -42,24 +45,19 @@ class TasksetGenerator:
 
     def generate(self, filename):
         """
-        1. Randomise n numbers from 0 to 1  - continuous uniform distribution (or shld use a normal distribution instead?)
+        1. Randomise n numbers from 0 to 1  - continuous uniform distribution or normal distribution
         2. Sum them up to find the current "magnitude", then normalize/scale to the expected magnitude (e.g. 50% of 200ms)
         3. Round to the precision
-        4. Generate task set
-
-        Problem: execution times are too uniform, we want some tasks with very large execution times. find a different distribution?
-        > either normal distribution, or
-        > Pareto distribution, or
-        > take the sum and divide it into the number of desired parts?
+        4. Determine a deadline for each task (would only be used by EnSuRe)
+        5. Generate the task list
         """
         # 1. Randomly sample n numbers
-        rand_sample = np.random.normal(loc=self.mean, scale=self.sd, size=self.n)
-        # normalize to 0 to 1
-        rand_sample = (1-self.min_norm)*(rand_sample - np.min(rand_sample))/np.ptp(rand_sample) + self.min_norm
-
-        # got one task whose execution time is 0
-
-        #rand_sample = np.random.random_sample(n)
+        if self.distribution == "uniform":
+            rand_sample = np.random.random_sample(n)
+        elif self.distribution == "normal":
+            rand_sample = np.random.normal(loc=self.mean, scale=self.sd, size=self.n)
+            # normalize to 0 to 1
+            rand_sample = (1-self.min_norm)*(rand_sample - np.min(rand_sample))/np.ptp(rand_sample) + self.min_norm
 
         # 2. Get the "magnitude" of the array (sum of the array)
         magnitude = sum(rand_sample)
@@ -67,7 +65,26 @@ class TasksetGenerator:
         # 3. Scale the samples to the expected magnitude
         exec_times = rand_sample * (self.target_magnitude / magnitude)
 
-        # 4. Generate the task data
+        # 4. Determine a deadline for each task
+        # NOTE: one way to guarantee WQ: split deadlines into equal parts
+        # then, if any task's deadline is smaller than its execution time, increase it 
+        deadlines = []
+        time_window = self.frame_duration / self.n
+        half_time_window = time_window * 0.5
+        for i in range(len(exec_times)):
+            #deadline = np.random.uniform(exec_time, self.frame_duration)    # deadline is anywhere between end of task execution, and frame_duration
+            
+            # introduce a small gaussian distribution to vary the deadline
+            deadline = min(i * time_window + half_time_window * np.random.normal(), self.frame_duration)
+            # if any task's deadline is smaller than its execution time, increase it 
+            while deadline < exec_times[i]:
+                deadline += min(exec_times[i], self.frame_duration)
+
+            deadlines.append(deadline)
+
+        # NOTE: how to ensure WQ must be within?
+
+        # 5. Generate the task data
         tasks = ""
         lpexec_check = []
         hpexec_check = []
@@ -83,11 +100,12 @@ class TasksetGenerator:
 
             # iii. HP execution time (rounded to precision)
             hp_exec = round(lp_exec * self.lp_hp_ratio, self.precision)
-            tasks = tasks + str(hp_exec)
+            tasks = tasks + str(hp_exec) + ","
 
             hpexec_check.append(hp_exec)
 
             # iii. task deadline (for EnSuRe only)
+            tasks = tasks + str(deadlines[i])
 
             # iv. optional execution time (for EnSuRe only)
 
